828a98894479c60a32ce957f98037429e221d583
admin dell <admin dell@DESKTOP-80HLJ8Q>
2022-07-19 11:44:07 +0530

    MavenProject

---
+++ NEW_SELENIUM_FRAMEWORK/src/test/java/org/practise/Pactise1.java
@@ -0,0 +1,30 @@
+package org.practise;
+
+import org.testng.annotations.Test;
+
+public class Pactise1 
+{
+
+	@Test
+	public void test5()
+	{
+		System.out.println("Test5 Passed");
+	}
+	@Test
+	public void test6()
+	{
+		System.out.println("Test6 Passed");
+		
+	}
+	@Test
+	public void test7()
+	{
+		System.out.println("Test7 Passed");
+	}
+	@Test
+	public void test8()
+	{
+		System.out.println("Test8 Passed");
+	}
+
+}
---
+++ NEW_SELENIUM_FRAMEWORK/src/test/java/org/practise/Practise.java
@@ -0,0 +1,38 @@
+package org.practise;
+
+import org.testng.annotations.Test;
+
+public class Practise 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Test1 Passed");
+		String browser = System.getProperty("BROWSER");
+		String url = System.getProperty("URL");
+		String username = System.getProperty("USERNAME");
+		String password = System.getProperty("PASSWORD");
+		
+		System.out.println("browser--------->"+browser);
+		System.out.println("url--------->"+url);
+		System.out.println("username--------->"+username);
+		System.out.println("password--------->"+password);
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("Test2 Passed");
+		
+	}
+	@Test
+	public void test3()
+	{
+		System.out.println("Test3 Passed");
+	}
+	@Test
+	public void test4()
+	{
+		System.out.println("Test4 Passed");
+	}
+
+}
